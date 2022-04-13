4e746213070b802613d3a4ca8660d20d0b6dd07e
v.markitanov <Dc58dTWO>
2022-04-13 12:54:32 +0300

    QML

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
-#include "src/MainWindow.h"
-#include <QApplication>
+//#include "src/MainWindow.h"
+//#include <QPushButton>
+
+#include <QGuiApplication>
+#include <QIcon>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
 int main(int argc, char *argv[]) {
-    QApplication::setStyle("Fusion");
-    QApplication a(argc, argv);
-    QApplication::setWindowIcon(QIcon(":images/ava.png"));
-    MainWindow mainWindow;
-    mainWindow.show();
+    QQuickStyle::setStyle("Fusion");
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    return QApplication::exec();
+    QGuiApplication app(argc, argv);
+    QGuiApplication::setWindowIcon(QIcon(":images/ava.png"));
+
+    QQmlApplicationEngine engine;
+    const QUrl url("qrc:qml/main.qml");
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                if (!obj && url == objUrl)
+                    QCoreApplication::exit(-1);
+            }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return QGuiApplication::exec();
 }
---
+++ resources/qml/main.qml
@@ -0,0 +1,21 @@
+import QtQuick 2.12
+import QtQuick.Window 2.12
+import QtQuick.Controls 2.12
+
+Window {
+    id: win
+    visible: true
+    width: 640
+    height: 480
+    title: qsTr("Hello World")
+
+    Button {
+        id: button1
+        text: "Click me"
+        anchors.centerIn: parent
+
+        onClicked: {
+           win.color = Qt.rgba(Math.random(),Math.random(),Math.random())
+        }
+    }
+}
